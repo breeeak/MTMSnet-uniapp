8c63db109f660da6cf7d9ec46e6dea7bbcfec64c
Shuo Meng <35834887+breeeak@users.noreply.github.com>
2020-07-08 22:26:48 +0800

    initial commit

---
+++ pages/index/index.vue
@@ -0,0 +1,96 @@
+<template>
+	<view class="content">
+		<!-- #ifndef MP -->
+		<view class="status_bar">
+			<!-- 这里是状态栏  空出-->
+		</view>
+		<!-- #endif -->
+		<view class="solid-top padding   flex justify-between">
+			<view class="action">
+				<text class="cuIcon-title text-orange">检测进度</text>
+			</view>
+			<view>
+				<text class="cuIcon-question text-green">操作帮助</text>
+			</view>
+		</view>
+		<view class="padding solid-bottom">
+			<view class="cu-steps">
+				<view class="cu-item" :class="index>num?'':'text-blue'" v-for="(item,index) in numList" :key="index">
+					<text class="num" :class="index==3?'err':''" :data-index="index + 1"></text> {{item.name}}
+				</view>
+			</view>
+		</view>
+		<view class="bg-white">
+			<view class="solid-top solid-bottom padding  flex justify-between">
+				<view class="action">
+					<text class="cuIcon-picfill text-orange">原图</text>
+				</view>
+				<view>
+					<text class="cuIcon-refresh text-green">重新选择</text>
+				</view>
+			</view>
+			<view class="image">
+				<image src="../../static/textileImg/20190218-213245-174.png" mode="widthFix" style="width: 750rpx;"></image>
+			</view>
+			<view class="solid-top solid-bottom padding flex justify-between">
+				<view class="action">
+					<text class="cuIcon-creativefill text-orange">识别结果</text>
+				</view>
+				<view>
+					<text class="cuIcon-same text-green">查看中间结果</text>
+				</view>
+			</view>
+			
+			<view class="cu-form-group">
+				<view class="title">经密:</view>
+				<input placeholder="143 根/英寸" name="input"></input>
+				<text class='cuIcon-locationfill text-green text-sm' style="font-size: 24rpx;">反馈错误</text>
+			</view>
+			<view class="cu-form-group">
+				<view class="title">纬密:</view>
+				<input placeholder="122 根/英寸" name="input"></input>
+				<text class='cuIcon-locationfill text-green text-sm' style="font-size: 24rpx;">反馈错误</text>
+			</view>
+			<view class="cu-form-group">
+				<view class="title">组织:</view>
+				<input placeholder="" name="input"></input>
+				<text class='cuIcon-locationfill text-green text-sm' style="font-size: 24rpx;">反馈错误</text>
+			</view>
+			<view class="image">
+				<image src="../../static/textileImg/232332.png" mode="widthFix" style="width: 750rpx;"></image>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				numList: [{
+					name: '选择图片'
+				}, {
+					name: '上传识别'
+				}, {
+					name: '识别结果'
+				}, {
+					name: '结果反馈'
+				}, ],
+				num: 2,
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.status_bar {
+		height: $navigation-bar-height+40upx;
+		width: 100%;
+	}
+</style>
---
+++ pages/statistics/calendar.vue
@@ -0,0 +1,192 @@
+<template>
+	<view>
+		<model-calendar
+		:sendYear="toYear" :sendMonth="toMonth"
+		:dataSource="signData" :totalNum="sumCount"
+		 @dateChange="getData" @clickChange="clickRegister"	 >
+		 </model-calendar>
+		 
+		 <view class='count'>
+		 	<text>已坚持打卡</text>
+		 	<view class='daynumber'>
+		 		<text class='number'>{{sumCount}}</text>
+		 		<text class='day'>天</text>
+		 	</view>
+		 	<view>本月累积打卡<text class="monthSum">{{signData.length}}</text>天</view>
+		 	<text>再接再厉，继续努力!</text>
+		 </view>
+		 <view v-if="signFlag" class="padding flex flex-direction">
+		 	<button class="cu-btn bg-gradual-green margin-tb-sm lg" @click="shareEvn">今日签到</button>
+		 </view>
+		 
+		 <view class="share-pro" >
+		 	<view class="share-pro-mask" v-if="deliveryFlag"></view>
+		 	<view class="share-pro-dialog" :class="deliveryFlag?'open':'close'" >
+		 		<view class="close-btn" @tap="closeShareEvn">
+		 			<span class="font_family">&#xe6e6;</span>
+		 		</view>
+		 		<view class="share-pro-title">分享</view>
+		 		<view class="share-pro-body">
+		 			<view class="share-item">
+		 				<button open-type="share">
+		 					<view class="font_family share-icon">&#xe6fa;</view>
+		 					<view >分享给好友</view>
+		 				</button>
+		 			</view>
+		 			<view class="share-item" @tap="createCanvasImageEvn">
+		 				<view class="font_family share-icon">&#xe6f9;</view>
+		 				<view >生成分享图片</view>
+		 			</view>
+		 		</view>
+		 	</view>
+		 </view>
+		 <hchPoster ref="hchPoster" :canvasFlag.sync="canvasFlag" @cancel="canvasCancel" :posterObj.sync="posterData"/>
+		 <view :hidden="canvasFlag"><!-- 海报 要放外面放组件里面 会找不到 canvas-->
+		 	<canvas class="canvas"  canvas-id="myCanvas" ></canvas><!-- 海报 -->
+		 </view>
+		 
+	</view>
+</template>
+
+<script>
+	import modelCalendar from '@/components/Calendar.vue';
+	import hchPoster from '@/components/hch-poster/hch-poster.vue'
+	export default {
+		
+		data() {
+			return {
+				toYear: parseInt(new Date().getFullYear()), //本日
+				toMonth: parseInt(new Date().getMonth() + 1), //本月
+				sumCount: 0,
+				signFlag: true,
+				deliveryFlag: false,
+				canvasFlag:true,
+				signData: [],
+				posterData:{},
+			}
+		},
+		methods: {
+			//获取当前用户该任务的签到数组
+			getData(val) {
+				let y=val.split('-')[0];
+				let m=val.split('-')[1];
+				//this.$http.postHttp("Comment/GetRecord", {//可以通过后台接口去获取你的打卡数据
+				// 	Year: y,
+				// 	Month: m,
+				// }, (res) => {
+				//console.log(res);
+				this.sumCount = 88; //res.SumCount		
+				if (y == this.toYear && m == this.toMonth) {
+					let num=["02","03","06","08","12","15"],newSign=[],today=new Date().getDate();
+					
+					for(let i=0;i<6;i++){
+						if(parseInt(num[i])>today){
+							break;
+						}
+						newSign.push(y+"-"+m+"-"+num[i])
+					}
+					this.signData = newSign;
+				} else {
+				 	this.signData = [];
+				 }
+				// })
+			},
+			
+			// 分享弹窗
+			shareEvn() {
+				this.deliveryFlag = true;
+			},
+			// 关闭分享弹窗
+			closeShareEvn() {
+				this.deliveryFlag = false;
+			},
+			// 取消海报
+			canvasCancel(val){
+				this.canvasFlag=val;
+			},
+			createCanvasImageEvn(){
+				// 这个是固定写死的小程序码
+				Object.assign(this.posterData,
+				{
+					url:api.HOST+'/static/images/hb.jpg',//商品主图
+					icon:api.HOST+'/static/images/rank-select.png',//奖杯图标
+					title:"这是我的朗读成就，等你挑战哦",//标题
+					discountPrice:this.reads,//金币
+					orignPrice:this.scores,//积分
+					code:api.HOST+'/static/images/ha.jpg',//小程序码
+				})
+				this.$forceUpdate();//强制渲染数据
+				setTimeout(()=>{
+					this.canvasFlag=false;//显示canvas海报
+					this.deliveryFlag = false;//关闭分享弹窗
+					this.$refs.hchPoster.createCanvasImage();//调用子组件的方法
+				},500)				
+			},
+			
+			navTo(url) {
+				//TODO 使用时放开
+				// if (!this.hasLogin) {
+				// 	url = '/pages/public/login';
+				// }
+				uni.navigateTo({
+					url
+				})
+			},
+		},
+		created() {
+			//获取当前用户当前任务的签到状态  			
+			this.getData(this.toYear+"-"+this.toMonth);
+		},
+		components:{
+			modelCalendar,
+			hchPoster
+		}
+	}
+</script>
+
+<style>
+	.count .daynumber {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+
+	.count .daynumber .day {
+		margin-top: 50rpx;
+	}
+
+	.count {
+		margin: 20rpx;
+		padding: 30rpx;
+		display: flex;
+		text-align: center;
+		border-radius: 10px;
+		flex-direction: column;
+		justify-content: center;
+		background-color: #fff;
+		align-items: center;
+		
+	}
+
+	.count .number {
+		color: #fff;
+		font-size: 60rpx;
+		background-color: #94db98;
+		width: 100rpx;
+		height: 100rpx;
+		border-radius: 50%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		margin: 20rpx;
+	}
+
+	.monthSum {
+		color: red;
+		font-size: 40rpx;
+	}
+
+	.count text {
+		margin: 10rpx;
+	}
+</style>
